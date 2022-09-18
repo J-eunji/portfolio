536e6393b1d291b41aec369e32b911d6c5430b8d
J-eunji <109052764+J-eunji@users.noreply.github.com>
2022-09-18 18:09:17 +0900

    project 페이지 완성

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -4,9 +4,81 @@ import ProjectItem from "./ProjectItem";
 
 export default function Project() {
   const Item = [
-    { id: 1, link: "", text: "" },
-    { id: 2, link: "", text: "" },
-    { id: 3, link: "", text: "" },
+    {
+      id: 1,
+      img: "tmdb.png",
+      link: "https://j-eunji.github.io/tmdb/",
+      title: "영화 검색 사이트",
+      text: (
+        <>
+          <p>
+            <a href="https://www.themoviedb.org/?language=ko">TMDB</a>의 무료
+            API를 이용하여 미디어(영화, TV, 인물)검색 사이트를 구현했습니다.
+          </p>
+          <p>
+            React의 특성을 이용하여 같은 디자인의 컴포넌트들로 구성하여 깔끔하고
+            가독성있게 작업했습니다.
+          </p>
+          <p>
+            메인, 영화, TV, 인물 페이지 및 상세 페이지를 구성하여 편리하게
+            미디어 정보를 볼 수 있습니다."
+          </p>
+        </>
+      ),
+      git: (
+        <a href="https://github.com/J-eunji/tmdb.git">
+          https://github.com/J-eunji/tmdb.git
+        </a>
+      ),
+      use: "React, Recoil, CSS, JS",
+    },
+    {
+      id: 2,
+      img: "todolist.png",
+      link: "https://j-eunji.github.io/todolist/",
+      title: "투두리스트",
+      text: (
+        <>
+          <p>
+            react-calendar 라이브러리로 깔끔하고 이용하기 편리한 저만의
+            투두리스트를 만들어 보았습니다.
+          </p>
+          <p>
+            할 일을 작성하면 달력에 마크가 표시됩니다. 헤더에 남은 할 일의
+            갯수가 입력되고 할 일을 모두 완료하면 달력의 마크 색이 변합니다.
+            데이터를 로컬스토리지에 저장하여 새로고침해도 데이터를 유지할 수
+            있습니다.
+          </p>
+        </>
+      ),
+      git: (
+        <a href="https://github.com/J-eunji/todolist.git">
+          https://github.com/J-eunji/todolist.git
+        </a>
+      ),
+      use: "React, Recoil, CSS, JS",
+    },
+    {
+      id: 3,
+      img: "python.png",
+      link: "https://gravel-tulip-09d.notion.site/Python-501108a9bb644618b25d7f2dbd63b826",
+      title: "파이썬 미니 프로젝트 모음",
+      text: (
+        <>
+          <p>
+            python을 배울 때 만들었던 여러가지 미니 프로젝트들을 모아놓은 노션
+            페이지 입니다.
+          </p>
+          <p>
+            자판기, 타이핑 게임, 정보관리와 AI 딥러닝을 이용하여 동영상의 물체를
+            인식하는 프로그램 등이 있습니다. 처음 코딩을 접하고 만든 HTML
+            페이지로 제가 얼마나 발전했는지 알 수 있었습니다.
+          </p>
+        </>
+      ),
+      git: "노션 페이지 참조",
+      use: "Python",
+    },
   ];
   return (
     <Container>
@@ -33,9 +105,8 @@ const Box = styled.div`
   align-items: center;
   justify-content: space-between;
   margin-top: 50px;
-  width: 1400px;
+  width: 1500px;
   height: 600px;
   padding: 0 30px;
   position: relative;
-  background-color: rgba(0, 0, 0, 0.2);
 `;
--- src/components/Project/ProjectItem.js
+++ src/components/Project/ProjectItem.js
@@ -1,34 +1,64 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export default function ProjectItem() {
+export default function ProjectItem({ item }) {
+  const { link, text, img, title, use, git } = item;
+  console.log(img);
   return (
     <Container>
-      <ImgBox>img</ImgBox>
-      <Text>text</Text>
+      <ImgBox img={img}>
+        <a href={link} target="_blank">
+          <img src={require(`../../assets/imgs/${img}`)} alt={img} />
+        </a>
+      </ImgBox>
+      <Title>{title}</Title>
+      <Text>
+        <p>{text}</p>
+        <p>code: {git}</p>
+        <p>{use}</p>
+      </Text>
     </Container>
   );
 }
 
 const Container = styled.div`
-  width: 430px;
-  height: 550px;
-  padding: 10px;
-  background-color: rgba(0, 0, 0, 0.1);
+  width: 450px;
+  height: 570px;
+  padding: 20px;
+  border-radius: 8px;
+  background-color: rgba(0, 0, 0, 0.03);
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
   align-items: center;
+  a {
+    color: #000;
+  }
 `;
 
 const ImgBox = styled.div`
   width: 400px;
-  height: 260px;
-  background-color: rgba(0, 0, 0, 0.1);
+  height: 220px;
+  overflow: hidden;
   cursor: pointer;
+  img {
+    width: 400px;
+    height: 200px;
+    border-radius: 8px;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 1.3em;
+  font-weight: 700;
+  margin-bottom: 10px;
 `;
 
 const Text = styled.div`
   width: 400px;
-  height: 260px;
-  background-color: rgba(0, 0, 0, 0.1);
+  height: 280px;
+  word-spacing: 2.5px;
+  line-height: 30px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
